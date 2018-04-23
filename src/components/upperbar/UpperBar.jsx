29a2441944921413e5b3863f94bbcca98531129c
JensDeCraecker <jens.decraecker@student.ap.be>
2018-04-23 15:08:36 +0200

    Add new logo + add date + logo need to be fixed

--- src/components/upperbar/UpperBar.jsx
+++ src/components/upperbar/UpperBar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './upperbar.scss';
 
-import logo from '../../images/logo.svg';
+import moment from 'moment';
+
+import logo from '../../images/logo_ven.svg';
 
 const UpperBar = props => {
+  const date = moment().format('LLLL');
   return (
     <div className="upperbar">
       <img src={logo} alt="avendoo_logo" />
+      <h6>{date}</h6>
     </div>
   );
 };
