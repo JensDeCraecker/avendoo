0509e53212e12e169d64a7296eb38302c9154865
JensDeCraecker <jens.decraecker@student.ap.be>
2018-05-07 11:23:54 +0200

    Add BlueBar component + data file

---
+++ src/components/bluebar/BlueBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlueBar = props => {
+  return <div className="blue" />;
+};
+
+export default BlueBar;
--- src/scenes/bread/Bread.jsx
+++ src/scenes/bread/Bread.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import NavBar from '../../components/navbar/NavBar';
 import UpperBar from '../../components/upperbar/UpperBar';
+import BlueBar from '../../components/bluebar/BlueBar';
 
 const Bread = props => {
   return (
     <div className="view">
       <NavBar />
       <UpperBar />
-      <div className="blue" />
+      <BlueBar />
       <p> brood </p>
     </div>
   );
