2eb3ff8f2d5b6bf7d7d96a5da5952e8e7cd09ed3
JensDeCraecker <jens.decraecker@student.ap.be>
2018-05-09 10:36:48 +0200

    Add info to kader

--- src/components/dealbar/DealBar.jsx
+++ src/components/dealbar/DealBar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from '../button/Button';
+import BigInfo from '../biginfo/BigInfo';
 
 import './dealbar.css';
 
@@ -10,6 +11,11 @@ class DealBar extends Component {
       <div className="dealbar">
         <h2>2e brood gratis</h2>
         <h3>Bakker De Craecker</h3>
+        <div className="all">
+          <BigInfo sort="Wit" price="€1,5" />
+          <BigInfo sort="Grof" price="€1,5" />
+          <BigInfo sort="Bruin" price="€1,5" />
+        </div>
         <Button onClickCallback={this.togglePopup}> Melding </Button>
       </div>
     );
